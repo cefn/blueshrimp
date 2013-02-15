e07dd9b46e4a5d54d722e19122efb722c24d43e2
Cefn Hoile <cefn@cefn-precise-dell.default>
2013-02-15 14:03:26 +0000

    Reorganising folders

--- desktop/connectbluetooth.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Connects to a paired Serial Bluetooth device
-# to provide a serial connection for use by Arduino IDE 
-# or pyfirmata-based python scripts 
-# (proven with a JY-MCU baseboard and HC-06 module 
-sudo rfcomm release /dev/rfcomm0
-MACADDRESS="00:12:10:22:09:14"
-rfcomm connect /dev/rfcomm0 ${MACADDRESS}
